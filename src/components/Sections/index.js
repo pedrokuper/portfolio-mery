7736ac5eb5506b6a672a64466f23b85a4ffb1f2a
Pedro <pkupersmit@gmail.com>
2021-01-18 15:46:22 -0300

    deleted comment in sections component

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -22,31 +22,3 @@ function Sections({ sections }) {
 
 export default Sections;
 
-/* 
-
-import React from "react";
-import "./style.scss";
-import { Link, useParams, useRouteMatch } from "react-router-dom";
-
-function Sections({ sections }) {
-  const { sectionId } = useParams();
-  const { url, path } = useRouteMatch();
-  console.log("url", url, "path", path);
-  return (
-    <div className="section-wrapper">
-      {sections.map(({ title, img }, key) => {
-        const urlTitle = title.toLowerCase().replace(/\s/g, "-");
-        return (
-          <div key={key} className="section">
-            <Link to={`/seccion/${urlTitle}/${sectionId}`}>
-              <img className="img" src={img} alt={title} />
-            </Link>
-            <p className="title">{title.toUpperCase()}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-
-*/
