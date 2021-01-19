e214554079431e4b2e0cffd31db97420c9f575e3
Pedro <pkupersmit@gmail.com>
2021-01-19 17:12:17 -0300

    back arrow added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./style.scss";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  //  const url = window.location.href;
+
+  const history = useHistory();
+  const { pathname } = history.location;
+
+  function handleBackButton() {
+    history.goBack();
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-title">
+        <div className="navbar-back-arrow">
+          {pathname !== "/" && <BsArrowLeft onClick={handleBackButton} />}
+        </div>
         <Link to="/">
           <p>María Pirsch | Fotografía</p>
         </Link>
