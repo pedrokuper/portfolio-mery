e52d50b2aa1a40e4c7a917d05c59e3380eaef275
Pedro <pkupersmit@gmail.com>
2021-01-24 18:08:31 -0300

    sections media queries

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -5,24 +5,26 @@ import Section from "../Section";
 
 function Sections({ sections, urlPath }) {
   return (
-    <div className="section-wrapper">
-      {sections.map(({ title, img }, key) => {
-        return (
-          <div key={key} className="section">
-            {title === "DOCUMENTAL" ? (
-              <Link to={`/documental/`}>
-                <Section key={key} img={img} title={title} />
-              </Link>
-            ) : (
-              <Link
-                to={`${urlPath}/${title.toLowerCase().replace(/\s/g, "-")}`}
-              >
-                <Section key={key} img={img} title={title} />
-              </Link>
-            )}
-          </div>
-        );
-      })}
+    <div className="sections-wrapper">
+      <div className="sections-container">
+        {sections.map(({ title, img }, key) => {
+          return (
+            <div key={key} className="section">
+              {title === "DOCUMENTAL" ? (
+                <Link to={`/documental/`}>
+                  <Section key={key} img={img} title={title} />
+                </Link>
+              ) : (
+                <Link
+                  to={`${urlPath}/${title.toLowerCase().replace(/\s/g, "-")}`}
+                >
+                  <Section key={key} img={img} title={title} />
+                </Link>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
